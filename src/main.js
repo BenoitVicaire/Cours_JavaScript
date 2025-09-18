f6fd2934f2429b6309831440ba53992fb1e29259
BenoitV <Benoitvicaireformation@gmail.com>
2025-09-18 14:26:22 +0200

    Correction du len

--- src/main.js
+++ src/main.js
@@ -1,17 +1,15 @@
-import './style.css';
-import IMC from './imc.js';
-
-
+import "./style.css";
+import IMC from "./imc.js";
 
 let list = [
-  new IMC("Sébastien Chabal",135,1.7),
-  new IMC("Escaladeuse",45,1.68),
-  new IMC("J0J0",300,2),
-  new IMC("Gontrand",90,1.75),
-  new IMC("Colonel Clock",200,1.75),
-  new IMC("J0siane de la Vega",99,1.55)
+  new IMC("Sébastien Chabal", 135, 1.7),
+  new IMC("Escaladeuse", 45, 1.68),
+  new IMC("J0J0", 300, 2),
+  new IMC("Gontrand", 90, 1.75),
+  new IMC("Colonel Clock", 200, 1.75),
+  new IMC("J0siane de la Vega", 99, 1.55),
 ];
 
-for(let i=0;i<list.length;i++){
-  list[i].display_imc();
+for (const element of list) {
+  element.display_imc();
 }
