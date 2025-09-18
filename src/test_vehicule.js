df4e747cc1cee349339ab4b06f384eee783d1101
BenoitV <Benoitvicaireformation@gmail.com>
2025-09-18 10:32:10 +0200

    Ex_03 terminé

---
+++ src/test_vehicule.js
@@ -0,0 +1,19 @@
+import Vehicule from "./vehicule.js";
+
+
+const voiture = new Vehicule("Mercedes CLK",4,250);
+const moto = new Vehicule("Honda CBR",2,280);
+
+
+console.log(voiture.detect());
+console.log(moto.detect());
+
+console.log(voiture.vitesse);
+console.log(voiture.plusRapide(moto));
+voiture.boost();
+console.log(voiture.vitesse);
+console.log(moto);
+console.log(voiture);
+console.log(voiture.plusRapide(moto));
+
+
---
+++ src/vehicule.js
@@ -0,0 +1,34 @@
+export default class Vehicule{
+
+    //Constructeur
+    constructor(nom_Vehicule,nbr_Roue,vitesse){
+        this.nom=nom_Vehicule;
+        this.roues=nbr_Roue;
+        this.vitesse=vitesse;
+    }
+    //Méthode
+    detect(){
+        if (this.roues==2){
+            return "Moto";
+        }else if(this.roues==4) {
+            return "Voiture";
+        }else if (this.roues==1){
+            return "MonoCycle";
+        } else {
+            return "qu'est ce donc que ce drole de vehicule?";
+        }
+    };
+
+    boost(){
+        this.vitesse+=50;
+    }
+    plusRapide(vehicule){
+        if(this.vitesse > vehicule.vitesse){
+            return ` ${this.nom} est plus rapide que ${vehicule.nom}`;
+            
+        } 
+        else{
+            return `${vehicule.nom} est plus rapide que ${this.nom}`;
+        }
+    }
+}
