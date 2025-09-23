c1004acea41c5738e4897154f8ddc64ba13cc0bb
BenoitVicaire <Benoitvicaireformation@gmail.com>
2025-09-23 16:59:41 +0200

    fin de journée

--- src/annexe.js
+++ src/annexe.js
@@ -1,6 +1,6 @@
 export class compteBancaire {
     // Constructor
-    constructor(owner_name,balance){
+    constructor(owner_name,balance=0){
         this.owner_name=owner_name;
         this.balance=balance;
     }
@@ -10,15 +10,15 @@ export class compteBancaire {
             throw new Error("Votre solde n'est pas suffisant pour effectuer cette opération");
         }else if(amount<=0){
             throw new Error("Le montant que vous avez selectionné est invalide, veuillez saisir un montant strictement positif");
-        }else{
-            return this.balance-=amount;
         }
+        this.balance-=amount;
+        
     }
     deposit(amount){
         if(amount<=0){
             throw new Error("Le montant que vous avez selectionné est invalide, veuillez saisir un montant strictement positif");
-        }else
-        return this.balance+=amount;
+        }
+        this.balance+=amount;
     }
     transfer(amount,target){
         if(amount<=0){
@@ -26,10 +26,9 @@ export class compteBancaire {
         }else if(amount>this.balance){
             throw new Error("Votre solde n'est pas suffisant pour effectuer cette opération")
         }else if(typeof target =="undefined"){
-            throw new Error("Le beneficiaire du virrement n'existes pas")
-        }else{
-            this.balance-=amount;
-            target.balance+=amount;
+            throw new Error("Le beneficiaire du virement n'existes pas")
         }
+        this.balance-=amount;
+        target.balance+=amount;    
     }
 }
--- src/main.js
+++ src/main.js
@@ -1,28 +1,49 @@
 import "./style.css";
 import {compteBancaire} from "./annexe.js";
 
-const alex = new compteBancaire("Alex",1000);
-const marco = new compteBancaire("Marco",1000);
-const clovis = new compteBancaire("Clovis",1000);
+const account_name = document.getElementById("account_name");
+const bt_create = document.getElementById("bt_create");
+const id_account = document.getElementById("id_account");
+const id_amount = document.getElementById("id_amount");
+const bt_deposit = document.getElementById("bt_deposit");
+const bt_withdraw = document.getElementById("bt_withdraw");
+const id_source = document.getElementById("id_source");
+const id_target = document.getElementById("id_target");
+const id_transfert = document.getElementById("id_transfert");
+const bt_transfert = document.getElementById("bt_transfert");
 
-console.log(alex);
-try{
-    alex.withdraw(10000);
-    }catch(error){
-        console.log(error.message);
-}
-    console.log(alex);
-try{
-    marco.transfer(300,clovis);
-    }catch(error){
-        console.log(error.message);
-    }
-try{
-    alex.withdraw(1200);
-    }catch(error){
-        console.log(error.message);
-    }
+const account_list =[];
 
-    console.log(alex);
-    console.log(marco);
-    console.log(clovis);
+// Créer un nouveau compte et l'ajouter a la account_list
+bt_create.addEventListener("click", ()=>{
+    if(account_name.value.trim()==""){
+        throw new Error("Champs vide")
+    }else{
+            account_list.map(account=>{
+                if (account.owner_name===account_name.value){
+                    throw new Error("Le compte existe déja")
+                }
+            })
+            account_list.push(new compteBancaire(account_name.value,0));
+            console.log(account_list);
+        }
+    
+})
+
+// Deposer
+bt_deposit.addEventListener("click", ()=>{
+    const amount=Number(id_amount.value)
+    if(amount<=0){
+            throw new Error("Le montant que vous avez selectionné est invalide, veuillez saisir un montant strictement positif");
+        }else if(id_account.value.trim()==""){
+            throw new Error("Champs vide");
+        }else if (isNaN(amount)){
+            throw new Error("Montant invalide");
+        }else{
+            const account= account_list.find(acc => acc.owner_name===id_account.value);
+            if (!account){
+                throw new Error ("Ce compte n'existe pas")
+            }
+            account.deposit(amount);
+        }
+})
