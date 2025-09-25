bc517a3bf62bec024a74e29edfb21272f0a402be
BenoitVicaire <Benoitvicaireformation@gmail.com>
2025-09-25 11:52:13 +0200

    fixed 2em methode

--- src/main.js
+++ src/main.js
@@ -9,4 +9,14 @@ const meteo = fetch("https://api.openweathermap.org/data/2.5/weather?lon=1.44&la
     return data.json();
 });
 
-console.log(meteo);
+// Deuxième methode
+const apiMeteo = async () => {
+    return await fetch ('https://api.openweathermap.org/data/2.5/weather?lon=1.44&lat=43.6&appid=' + apiKey)
+        .then(response => {
+            return response.json();
+        })
+}
+
+apiMeteo().then(json => { // console.log deuxieme methode
+    console.log(json);
+});
